76a3a0cd2aef1490a36a490f3586d862d09d5e43
topline12 <ajlanikollari@gmail.com>
2026-03-04 20:01:19 +0100

    Update kartolina.py

--- kartolina.py
+++ kartolina.py
@@ -6,6 +6,26 @@ urim_per_mesuesit={
     "Yllka":"Ju falenderojme per dijet qe na jepni ne biologji",
     "Gladiola":"Ju falenderojme per dijet qe na jepni ne anglisht",
     "Andi":"Ju falenderojme per dijet qe na jepni ne muzike",
+    "Loreta":"Ju falenderojme per dijet qe na jepni ne gjeografi",
+    "Laura":"Ju falenderojme per dijet qe na jepni ne matematike",
+    "Rexhina":"Ju falenderojme per dijet qe na jepni ne fizike",
+    "Albana Bega":"Ju falenderojme per drejtimin e shkolles ne nje menyre te drejte",
+    "Albana Agalliu":"Ju falenderojme per mbeshtetjen dhe keshillat qe na jep",
+    "Aibana":"Ju falenderojme per njohurite qe na jep ne frengjisht",
+    "Majlinda":"Ju falenderojme per njohurite qe na jepni ne gjuhen shqipe",
+    "Luli":"Ju falenderojme per njohurite qe na jepni ne biologji dhe kimi",
+    "Genta":"Ju falenderojme per prezencen qe e ngrohte qe sjell ne shkolle",
+    "Arta":"Oficere, ju falenderojme per kontrollin dhe mbeshtjen qe na jep",
+    "Paçi":"Ju falenderojme per njohurite qe na jep ne biologji dhe kimi",
+    "Andon":"Ju falenderojme per njohurite qe na jep ne art pamor",
+    "Zeni":"Ju falenderojme per aktivitetet qe na zhvillon ne fiskulture",
+    "Avenir":"Ju falenderojme per aktivitetet qe na zhvillon ne fiskulture",
+    "Egla":"Ju falenderojme per njohurite qe na jepni ne gjuhe shqipe",
+    "Erida":"Ju falenderojme per njohurite qe na jepni ne qytetari dhe gjuhe shqipe",
+    "Vojsava":"Ju falenderojme per njohurite qe na jepni ne gjeografi dhe histori",
+    "Liza":"Ju falenderojme per njohurite qe na jepni ne matematike dhe fizike",
+    "Margarita":"Ju falenderojme per njohurite qe na jepni ne gjuhe shqipe",
+    "Anila":"Ju falenderojme per njohurite qe na jepni ne gjeografi dhe histori",
 }
 emri=st.text_input("Vendosni emrin tuaj: ")
 if st.button("Shfaq urimin 🌷"):
@@ -33,4 +53,5 @@ if st.button("Shfaq urimin 🌷"):
              </div>
 
              """, unsafe_allow_html=True)
+
 
